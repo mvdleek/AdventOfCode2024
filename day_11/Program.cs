549c0ae6205225b02f4af6d0d6b0f13e29c06808
Mark van der Leek <1816961+mvdleek@users.noreply.github.com>
2024-12-11 22:27:47 +0100

    chech

--- day_11/Program.cs
+++ day_11/Program.cs
@@ -1,5 +1,8 @@
 ﻿
 
+using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace day_11
 {
     // Advent of code 2024: https://adventofcode.com/2024/day/11
@@ -13,9 +16,89 @@ namespace day_11
     //
     internal class Program
     {
+        static long maxValue = 0;
+
+
         static void Main(string[] args)
         {
-            var input = File.ReadAllLines("input.txt")[0].Split(' ').Select(x => long.Parse(x)).ToArray();  // 200446
+            // diepte 50 = 4 minuten 6_900_581_362
+            // diepte 55 = 
+
+            var stoneList = File.ReadAllLines("input.txt")[0].Split(' ').Select(x => long.Parse(x)).ToArray();  // 200446
+            //var stoneList = new List<long>(10_000_000); //optimization 1: allocate memory 1 time for the list, no expensive memory realloctions needed
+            //var stoneList = new long[2_000_000_000];
+            //var stoneListCount = input.Length;
+            //for (int i = 0; i < input.Length; i++)
+            //{
+            //    stoneList[i] = input[i];
+            //}
+
+            var sw = new Stopwatch();
+            sw.Start();
+            long stoneCount = 0;
+
+            Parallel.ForEach(
+                     stoneList,                         // source collection
+                     () => (long)0,                           // thread local initializer
+                     (n, loopState, localSum) =>        // body
+                     {
+                             localSum += GetStoneCountFromValue(n, 55);
+
+                         //localSum += n;
+                         Console.WriteLine("Thread={0}, n={1}, localSum={2}", Thread.CurrentThread.ManagedThreadId, n, localSum);
+                         return localSum;
+                     },
+                     (localSum) => Interlocked.Add(ref stoneCount, localSum)                   // thread local aggregator
+                 );
+
+            //for (int i = 0; i < stoneList.Length; i++)
+            //{
+            //    stoneCount += GetStoneCountFromValue(stoneList[i], 25);
+            //}
+            sw.Stop();
+
+            Console.WriteLine($"Number of stones: {stoneCount}");
+            Console.WriteLine($"Duration = {sw.Elapsed}");
+        }
+
+
+        private static long GetStoneCountFromValue(long stoneValue, int depth)
+        {
+            if (depth == 0) 
+            { 
+                
+                return 1; 
+            }
+
+            //rule 1: If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
+            if (stoneValue == 0)
+            {
+                return GetStoneCountFromValue(1, depth-1);
+            }
+
+            //rule 2: If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+            var digitCount = (int)Math.Floor(Math.Log10(stoneValue) + 1);
+
+            if (int.IsEvenInteger(digitCount))
+            {
+                var modValue = (long)Math.Pow(10, digitCount / 2);
+                var leftPart = stoneValue / modValue;
+                var rightPart = stoneValue % modValue;
+
+                return GetStoneCountFromValue(leftPart, depth - 1) +
+                       GetStoneCountFromValue(rightPart, depth - 1)
+                    ;
+            }
+
+            //rule 3: If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.        
+            return GetStoneCountFromValue(stoneValue * 2024, depth - 1);
+        }
+
+        static void MainPart1(string[] args)
+        {
+            
+
+            var input = File.ReadAllLines("input_example.txt")[0].Split(' ').Select(x => long.Parse(x)).ToArray();  // 200446
             //var stoneList = new List<long>(10_000_000); //optimization 1: allocate memory 1 time for the list, no expensive memory realloctions needed
             var stoneList = new long[2_000_000_000];
             var stoneListCount = input.Length;
@@ -24,10 +107,11 @@ namespace day_11
                 stoneList[i] = input[i];
             }
 
-            for (var i = 0; i < 75; i++)
+            for (var i = 0; i < 25; i++)
             {
                 Blink(stoneList, ref stoneListCount);
                 Console.WriteLine($"stones after blink { i+1 } = { stoneListCount }");
+                Console.WriteLine($"max after blink { i+1 } = { maxValue }");
             }
 
             Console.WriteLine($"Number of stones: { stoneListCount }");
@@ -64,7 +148,7 @@ namespace day_11
 
                 if (stoneValue == 0) { continue; }
 
-                var digitCount = GetNumberOfDigitsIn(stoneValue);
+                var digitCount = (int)Math.Floor(Math.Log10(stoneValue) + 1);
 
                 if (int.IsEvenInteger(digitCount))
                 {
@@ -87,7 +171,7 @@ namespace day_11
             }
 
             //rule 2: If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-            var digitCount = GetNumberOfDigitsIn(stoneValue);
+            var digitCount = (int)Math.Floor(Math.Log10(stoneValue) + 1);
 
             if (int.IsEvenInteger(digitCount))
             {
@@ -102,31 +186,14 @@ namespace day_11
             }
 
             //rule 3: If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.        }
-            stoneList[stoneWriteIndex] = stoneValue * 2024; //bug 1: wrote 1024 instead of 2024 :-(
-            return 1;
-        }
+            var newValue = stoneValue * 2024;
+            stoneList[stoneWriteIndex] = newValue; //bug 1: wrote 1024 instead of 2024 :-(
 
-        private static int GetNumberOfDigitsIn(long stoneValue)
-        {
-            //
-            //return stoneValue.ToString().Length;
-
-            //optimization 2: do not use .ToString()
-            //1100 --> 4  if < 1000 = 3
-            // 150 --> 3  if <  100 = 2
-            //  12 --> 2  if <   10 = 1
-
-            long compareValue = 10;
-            int digitcount = 1;
-            while (true) 
-            {  
-                if (stoneValue < compareValue)
-                {
-                    return digitcount;
-                }
-                compareValue *= 10;
-                digitcount++;
+            if (newValue > maxValue)
+            {
+                maxValue = newValue;
             }
+            return 1;
         }
     }
 }
