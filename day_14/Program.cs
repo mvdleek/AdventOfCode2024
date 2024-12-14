397bb090a3d34b1819ef9ff7fa4ee4a44e33d111
Mark van der Leek <wmvdleek@hotmail.com>
2024-12-14 09:23:13 +0100

    day 14

---
+++ day_14/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace day_14
+{
+    // input 500 bots, field 101x103, after 100 seconds
+    // p=0,4 v=3,-3  v=per second
+    // calculate safety factor: multiple robots in each of 4 quadrants
+    // quadrant=50x50
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
